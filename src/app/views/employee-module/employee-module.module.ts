5de2e2e1ec791fddf70f4be1d91d725415a489b8
Amal Somaweera <amalucsc@gmail.com>
2020-04-04 22:27:37 +0530

    test

--- src/app/views/employee-module/employee-module.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { RegistrationComponent } from "./registration/registration.component";
-
-@NgModule({
-  declarations: [RegistrationComponent, EmployeeViewComponent],
-  imports: [CommonModule]
-})
-export class EmployeeModuleModule {}
